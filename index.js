860f56f7586c7ea6caece959417ffcf1cff9b97e
Ryze3606 <vincenzosell200010@gmail.com>
2022-06-02 13:47:48 +0200

    menù

--- index.js
+++ index.js
@@ -20,3 +20,36 @@ client.on("messageCreate", message => {
             message.channel.send({embeds: [embedme]});
     }
 })
+
+
+//MENU//
+client.on("messageCreate", message => {
+    if(message.content == "!help"){
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Menù Help")
+        .setPlaceholder("Seleziona una pagni")
+        .setMinValues(1)
+        .setMaxValues(1)
+        .addOptions([
+            {
+                label: "Pagina 1",
+                description: "Vai alla pagina numero 1",
+                value: "pagina1"
+            },
+            {
+                label: "Pagina 2",
+                description: "Vai alla pagina numero 2",
+                value: "pagina2"
+            },
+            {
+                label: "Pagina 3",
+                description: "Vai alla pagina numero 3",
+                value: "pagina3"
+            }
+        ])
+        let row = new Discord.MessageActionRow()
+        .addComponents(select)
+
+        message.channel.send({ embeds: [embed], components: [row] })
+    }
+})
