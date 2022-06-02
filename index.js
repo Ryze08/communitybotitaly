d96888c4b5f0cacbf97ac25f66efd90fe8225aec
Ryze3606 <vincenzosell200010@gmail.com>
2022-06-02 12:44:59 +0200

    comando embed

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] })
 
 client.login(process.env.token)
+
+
+//MESSAGGIO IN EMBED//
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!embed")) {
+            var args = message.content.split(/\s+/);
+            var testo;
+            testo = args.slice(1).join(" ");
+
+            var embedme = new Discord.MessageEmbed()
+                .setColor("#ed00ff")
+                .setDescription(` ${testo}`) //Testo
+                .setTimestamp()
+                .setFooter ({ text: "Ryze Community"});
+            message.delete()
+            message.channel.send({embeds: [embedme]});
+    }
+})
