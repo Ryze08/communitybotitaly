ab3d3929c01c9001c6c17a7e942a8c7173012182
Ryze3606 <vincenzosell200010@gmail.com>
2022-06-02 12:32:17 +0200

    Upload files

---
+++ index.js
@@ -0,0 +1,4 @@
+const Discord = require("discord.js")
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] })
+
+client.login("OTgxODY0MzAzODIzMDQ0NjY5.Girugi.U25Km_arMhA5CEzHgsBMuOF1vCAnrSYqxT7oc8")
